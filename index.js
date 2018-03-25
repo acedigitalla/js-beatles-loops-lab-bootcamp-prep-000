c1cf2db633820287af78904033f4955c57ab6dce
Alex Edwards <info@acedigitalagency.net>
2018-03-25 22:23:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const facts = [
 function johnLennonFacts() {
   var i = 0;
   while (i < 4) {
-    facts[i] = ${facts[i]} + "!!!";
+    facts[i] = `${facts[i]} + !!!`
     i++;
  }
   return facts;
